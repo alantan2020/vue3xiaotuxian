e151c7ea0a7cd9091dfd0ebe36ac043298c66934
alantan2020 <582704597@qq.com>
2024-03-07 20:46:23 +0800

    第一次上传

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import Edit from './components/Edit.vue'
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+// TODO: 列表渲染
+const list = ref([])
+const getList = async () => {
+  const res = await axios.get('/list')
+  list.value =res.data
+}
+onMounted(() => getList())
+
+// TODO: 删除功能
+const onDelete = async (id) => {
+  await axios.delete(`/del/${id}`)
+  getList()
+}
+
+// TODO: 编辑功能
+const editRef = ref(null)
+const onEdit = (row) => {
+  editRef.value.open(row)
+}
+
+</script>
+
+<template>
+  <div class="app">
+    <el-table :data="list">
+      <el-table-column label="ID" prop="id"></el-table-column>
+      <el-table-column label="姓名" prop="name" width="150"></el-table-column>
+      <el-table-column label="籍贯" prop="place"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <template #default="{ row }">
+          <el-button type="primary" @click="onEdit(row)" link>编辑</el-button>
+          <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+  <Edit  ref="editRef"/>
+</template>
+
+<style scoped>
+.app {
+  width: 980px;
+  margin: 100px auto 0;
+}
+</style>
